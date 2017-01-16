145aa834c49673588fa73fcc52e8097c9b7fe531
Stefan Penner <stefan.penner@gmail.com>
2017-01-16 00:09:55 -0800

    add persistent cache

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
-
+let HASH;
 module.exports = {
   depsFromFile: require('./lib/deps-from-file'),
   depsFromSource: require('./lib/deps-from-source'),
-  depFilesFromFile: require('./lib/dep-files-from-file')
+  depFilesFromFile: require('./lib/dep-files-from-file'),
+  get _hash() {
+    return (HASH = HASH || require('hash-for-dep')('./'))
+  }
 };
