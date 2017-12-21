0da0e4fd00de82afa6e444540d193d22c2d03c7d
Stefan Penner <stefan.penner@gmail.com>
2017-12-21 11:08:22 -0800

    ensure `depsFromAST` is exported

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 module.exports = {
   depsFromFile: require('./lib/deps-from-file'),
   depsFromSource: require('./lib/deps-from-source'),
+  depsFromAST: require('./lib/deps-from-ast'),
   depFilesFromFile: require('./lib/dep-files-from-file'),
 };
